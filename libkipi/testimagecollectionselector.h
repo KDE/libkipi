b4b8e030956840dca6c08cbfaafd7e8f64e35274
Aurélien Gâteau <agateau@kde.org>
2004-07-21 22:08:09 +0000

    Added copyrights.
    svn path=/trunk/kdeextragear-libs-1/libkipi/; revision=331628

--- libkipi/testimagecollectionselector.h
+++ libkipi/testimagecollectionselector.h
@@ -1,3 +1,24 @@
+/* ============================================================
+ * File   : testimagecollectionselector.h 
+ * Authors: KIPI team developers (see AUTHORS files for details)
+ *	    
+ * Date   : 2004-07
+ * Description :
+ *
+ * Copyright 2004 by the KIPI team
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published bythe Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
 #ifndef TESTIMAGECOLLECTIONSELECTORWINDOW_H
 #define TESTIMAGECOLLECTIONSELECTORWINDOW_H
 
