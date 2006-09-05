ab69347cbafda75a4a51e381d3249585aaf85835
Gilles Caulier <caulier.gilles@gmail.com>
2006-09-05 14:15:54 +0000

    The libkipi API has changed. Need to increase release version.
    svn path=/trunk/extragear/libs/libkipi/; revision=581161

--- libkipi/version.h
+++ libkipi/version.h
@@ -23,9 +23,9 @@
 #ifndef KIPI_VERSION_H
 #define KIPI_VERSION_H
 
-static const char kipi_version[] = "0.1.4";
+static const char kipi_version[] = "0.1.5";
 
-#define KIPI_VERSION 0x000104
+#define KIPI_VERSION 0x000105
 
 #endif // KIPI_VERSION_H
 
