50a7042da02a524536afcdafc15df06c52333d0c
Gilles Caulier <caulier.gilles@gmail.com>
2007-05-29 20:05:15 +0000

    fix header
    svn path=/trunk/extragear/libs/libkipi/; revision=669609

--- libkipi/batchprogressdialog.cpp
+++ libkipi/batchprogressdialog.cpp
@@ -1,25 +1,24 @@
-//////////////////////////////////////////////////////////////////////////////
-//
-//    BATCHPROGRESSDIALOG.CPP
-//
-//    Copyright (C) 2004 Gilles Caulier <caulier dot gilles at gmail dot com>
-//
-//    This program is free software; you can redistribute it and/or modify
-//    it under the terms of the GNU Library General Public License as published by
-//    the Free Software Foundation; either version 2 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU Library General Public License for more details.
-//
-//    You should have received a copy of the GNU Library General Public License
-//    along with this program; if not, write to the Free Software
-//    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-//
-//////////////////////////////////////////////////////////////////////////////
-
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2004-05-04
+ * Description : Batch progress dialog
+ *
+ * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
 
 // Include files for Qt
 
@@ -65,6 +64,7 @@
 // Local includes
 
 #include "batchprogressdialog.h"
+#include "batchprogressdialog.moc"
 
 namespace KIPI
 {
@@ -220,5 +220,3 @@ void BatchProgressDialog::setProgress(int current, int total)
 }
 
 }  // NameSpace KIPI
-
-#include "batchprogressdialog.moc"
--- libkipi/batchprogressdialog.h
+++ libkipi/batchprogressdialog.h
@@ -1,24 +1,24 @@
-//////////////////////////////////////////////////////////////////////////////
-//
-//    BATCHPROGRESSDIALOG.H
-//
-//    Copyright (C) 2004 Gilles Caulier <caulier dot gilles at gmail dot com>
-//
-//    This program is free software; you can redistribute it and/or modify
-//    it under the terms of the GNU Library General Public License as published by
-//    the Free Software Foundation; either version 2 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU Library General Public License for more details.
-//
-//    You should have received a copy of the GNU Library General Public License
-//    along with this program; if not, write to the Free Software
-//    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, US
-//
-//////////////////////////////////////////////////////////////////////////////
+/* ============================================================
+ *
+ * This file is a part of kipi-plugins project
+ * http://www.kipi-plugins.org
+ *
+ * Date        : 2004-05-04
+ * Description : Batch progress dialog
+ *
+ * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * ============================================================ */
 
 /** @file batchprogressdialog.h */
 
@@ -29,6 +29,7 @@
 
 #include <kdialogbase.h>
 #include "libkipi/libkipi_export.h"
+
 class KListView;
 class KProgress;
 
