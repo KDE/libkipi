a51bed01fb9b41e5b20b4644ddeb0b1bd778c9ba
Gilles Caulier <caulier.gilles@gmail.com>
2007-06-05 14:21:29 +0000

    port to CMAke
    svn path=/trunk/extragear/libs/libkipi/; revision=671781

--- libkipi.pc.cmake
+++ libkipi.pc.cmake
@@ -1,11 +1,12 @@
-prefix=@prefix@
-exec_prefix=@exec_prefix@
-libdir=@libdir@
-includedir=@includedir@
+prefix=${CMAKE_INSTALL_PREFIX}
+exec_prefix=${BIN_INSTALL_DIR}
+libdir=${LIB_INSTALL_DIR}
+includedir=${INCLUDE_INSTALL_DIR}
 
 Name: libkipi
-Description: KDE library for shared plugins between graphical applications
+Description: KDE library for shared plugins between graphical photo management applications
+URL: http://www.kipi-plugins.org
 Requires:
 Version: 0.2.0
-Libs: -L${libdir} -lkipi
-Cflags: -I${includedir}
+Libs: -L${LIB_INSTALL_DIR} -lkipi
+Cflags: -I${INCLUDE_INSTALL_DIR}
