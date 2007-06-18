51c5ceb7e6064c98f153d9b24c6064d05b9bc44c
Laurent Montel <montel@kde.org>
2007-06-18 21:02:52 +0000

    Regenerate it
    svn path=/trunk/extragear/libs/libkipi/; revision=677296

--- libkipi/libkipi_export.h
+++ libkipi/libkipi_export.h
@@ -1,45 +1,40 @@
-/* ============================================================
- *
- * This file is a part of kipi-plugins project
- * http://www.kipi-plugins.org
- *
- * Date        : 2005-09-15
- * Description : KIPI library interface for KDE
- *
- * Copyright (C) 2005-2007 by Laurent Montel <montel@kde.org>
- *
- * This program is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General
- * Public License as published by the Free Software Foundation;
- * either version 2, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * ============================================================ */
+/*  This file is part of the KDE project
+    Copyright (C) 2007 David Faure <faure@kde.org>
 
-#ifndef _LIBKIPI_EXPORT_H
-#define _LIBKIPI_EXPORT_H
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef LIBKIPI_EXPORT_H
+#define LIBKIPI_EXPORT_H
+
+/* needed for KDE_EXPORT and KDE_IMPORT macros */
 #include <kdemacros.h>
 
-#ifdef Q_WS_WIN
-
 #ifndef LIBKIPI_EXPORT
-# ifdef MAKE_KIPI_LIB
+# if defined(MAKE_KIPI_LIB)
+   /* We are building this library */ 
 #  define LIBKIPI_EXPORT KDE_EXPORT
 # else
+   /* We are using this library */ 
 #  define LIBKIPI_EXPORT KDE_IMPORT
 # endif
 #endif
 
-#else // not windows
-
-#define LIBKIPI_EXPORT KDE_EXPORT
+# ifndef LIBKIPI_EXPORT_DEPRECATED
+#  define LIBKIPI_EXPORT_DEPRECATED KDE_DEPRECATED LIBKIPI_EXPORT
+# endif
 
 #endif
-
-#endif /* _LIBKIPI_EXPORT_H */
-
