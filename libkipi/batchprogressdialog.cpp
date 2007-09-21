e2800cc586610499823c7b3267b59d3fadb8c64b
Gerhard Kulzer <gerhard@kulzer.net>
2007-09-21 06:45:33 +0000

    i18n fix
    svn path=/trunk/extragear/libs/libkipi/; revision=715053

--- libkipi/batchprogressdialog.cpp
+++ libkipi/batchprogressdialog.cpp
@@ -109,14 +109,14 @@ BatchProgressDialog::BatchProgressDialog( QWidget *parent, const QString &captio
 
     m_actionsList = new QListWidget( box );
     m_actionsList->setSortingEnabled(false);
-    m_actionsList->setWhatsThis( i18n("<p>This is the current tasks list released.</p>") );
+    m_actionsList->setWhatsThis( i18n("<p>This is the current processing status.</p>") );
 
     //---------------------------------------------
 
     m_progress = new QProgressBar( box );
     m_progress->setRange(0, 100);
     m_progress->setValue(0);
-    m_progress->setWhatsThis( i18n("<p>This is the list current percent task released.</p>") );
+    m_progress->setWhatsThis( i18n("<p>This is the batch job progress in percentage.</p>") );
     resize( 600, 400 );
 }
 
