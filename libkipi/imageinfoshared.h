99648ca1f677d15538f69cab729a1fccfabe5c38
Gilles Caulier <caulier.gilles@gmail.com>
2008-02-08 08:38:54 +0000

    new virtual method to remove image attribute from kipi host. Will be used by GPSSync kipi plugins
    svn path=/trunk/extragear/libs/libkipi/; revision=772271

--- libkipi/imageinfoshared.h
+++ libkipi/imageinfoshared.h
@@ -6,7 +6,7 @@
  * Date        : 2004-02-19
  * Description : image info shared.
  *
- * Copyright (C) 2004-2007 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2004-2008 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2004-2005 by Renchi Raju <renchi.raju at kdemail.net>
  * Copyright (C) 2004-2005 by Jesper K. Pedersen <blackie at kde.org>
  * Copyright (C) 2004-2005 by Aurelien Gateau <aurelien dot gateau at free.fr>
@@ -62,6 +62,7 @@ namespace KIPI
         virtual QMap<QString,QVariant> attributes() = 0;
         virtual void clearAttributes() = 0;
         virtual void addAttributes( const QMap<QString,QVariant>& ) = 0;
+        virtual void delAttributes( const QMap<QString,QVariant>& ) = 0;
 
         virtual KUrl path();
         virtual QDateTime time( KIPI::TimeSpec spec );
