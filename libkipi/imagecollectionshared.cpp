fbaec5b391787b7103e5491cdd5fde82edd7a632
Gilles Caulier <caulier.gilles@gmail.com>
2008-09-17 09:43:58 +0000

    use kDebug
    svn path=/trunk/KDE/kdegraphics/libs/libkipi/; revision=861839

--- libkipi/imagecollectionshared.cpp
+++ libkipi/imagecollectionshared.cpp
@@ -15,12 +15,12 @@
  * and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation;
  * either version 2, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * ============================================================ */
 
 // KDE Includes.
@@ -49,7 +49,7 @@ void ImageCollectionShared::removeRef()
     _count--;
     if ( _count == 0 ) 
     {
-        // qDebug("Deleting!");
+        // kDebug(51000) << "Deleting!" << endl;
         delete this;
     }
 }
