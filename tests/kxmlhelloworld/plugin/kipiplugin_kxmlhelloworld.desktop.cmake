47f969557195defc95bb7470d744954fadbd6fd3
Script Kiddy <scripty@kde.org>
2013-03-16 09:34:21 +0100

    SVN_SILENT made messages (.desktop file)

--- tests/kxmlhelloworld/plugin/kipiplugin_kxmlhelloworld.desktop.cmake
+++ tests/kxmlhelloworld/plugin/kipiplugin_kxmlhelloworld.desktop.cmake
@@ -40,6 +40,7 @@ Name[zh_TW]=KXMLHelloWorld
 Comment=A demo kipi tool using KDE XML-GUI technology
 Comment[ca]=Una demostració de l'eina kipi que utilitza la tecnologia XML-GUI del KDE
 Comment[ca@valencia]=Una demostració de l'eina kipi que utilitza la tecnologia XML-GUI del KDE
+Comment[cs]=Ukázkový nástroj kipi využívající technologii KDE XML-GUI
 Comment[da]=Et demo KIPI-værktøj der bruger KDE XML-GUI-teknologi
 Comment[de]=Demo eines KIPI-Moduls. das die KDE-Technologie XML-GUI verwendet
 Comment[el]=Ένα εργαλείο επίδειξης kipi με χρήση τεχνολογίας KDE XML-GUI
