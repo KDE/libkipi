d8b70cf6266d4feeb7c5e9d4741f88b7435cdbed
Gilles Caulier <caulier.gilles@gmail.com>
2015-12-29 23:48:30 +0059

    fix (c)

--- tests/kxmlkipicmd/main.cpp
+++ tests/kxmlkipicmd/main.cpp
@@ -9,7 +9,7 @@
  *
  * @author Copyright (C) 2009-2010 by Michael G. Hansen
  *         <a href="mailto:mike at mghansen dot de">mike at mghansen dot de</a>
- * @author Copyright (C) 2011-2015 by Gilles Caulier
+ * @author Copyright (C) 2011-2016 by Gilles Caulier
  *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
  * @author Copyright (C) 2012 by Victor Dodon
  *         <a href="mailto:dodonvictor at gmail dot com">dodonvictor at gmail dot com</a>
