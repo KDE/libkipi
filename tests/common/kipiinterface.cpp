53f232aee1a4cd2b51bf7cb75c7e025a571e84fc
Gilles Caulier <caulier.gilles@gmail.com>
2016-02-10 07:18:38 +0059

    compile

--- tests/common/kipiinterface.cpp
+++ tests/common/kipiinterface.cpp
@@ -230,7 +230,8 @@ bool saveImage(const QUrl& url, const QString& format,
                const QByteArray& data, uint width, uint height,
                bool  sixteenBit, bool hasAlpha, bool* cancel)
 {
-    
+    // TODO
+    return false
 }
 
 // ---------------------------------------------------------------------------------------
