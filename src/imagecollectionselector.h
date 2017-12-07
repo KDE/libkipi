ad30cefa972ade6617bd77900ca3fc3999c41b79
Gilles Caulier <caulier.gilles@gmail.com>
2017-12-07 11:03:08 +0100

    bump dates

--- src/imagecollectionselector.h
+++ src/imagecollectionselector.h
@@ -7,7 +7,7 @@
  * @date   2008-02-22
  * @brief  image collection selector template
  *
- * @author Copyright (C) 2008-2017 by Gilles Caulier
+ * @author Copyright (C) 2008-2018 by Gilles Caulier
  *         <a href="mailto:caulier dot gilles at gmail dot com">caulier dot gilles at gmail dot com</a>
  *
  * This program is free software; you can redistribute it
--- tests/plugins/kxmlhelloworld/plugin_kxmlhelloworld.h
+++ tests/plugins/kxmlhelloworld/plugin_kxmlhelloworld.h
@@ -5,7 +5,7 @@
  * Date        : 2012-02-16
  * Description : an Hello World plugin using KDE XML-GUI technology.
  *
- * Copyright (C) 2012-2017 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2012-2018 by Gilles Caulier <caulier dot gilles at gmail dot com>
  * Copyright (C) 2012      by Victor Dodon   <dodonvictor at gmail dot com>
  *
  * This program is free software; you can redistribute it
